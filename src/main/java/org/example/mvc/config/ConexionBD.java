66606488bbda32a2372d52093e4e0b85b6b2ae5a
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-14 18:08:05 -0500

    commit-ConexionBD .

--- src/main/java/org/example/mvc/config/ConexionBD.java
+++ src/main/java/org/example/mvc/config/ConexionBD.java
@@ -1,4 +1,37 @@
 package org.example.mvc.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/productos_mvc";
+
+    private static final String USUARIO = "gtapiero";
+
+    private static final String PASSWORD = "Tapiero123";
+    public static Connection obtenerConexion() {
+
+        try {
+
+            Connection conexion = DriverManager.getConnection(
+                    URL,
+                    USUARIO,
+                    PASSWORD
+            );
+
+            System.out.println("Conexion exitosa a MySQL.");
+
+            return conexion;
+
+        } catch (SQLException e) {
+
+            System.out.println("Error al conectar con MySQL.");
+            System.out.println(e.getMessage());
+
+            return null;
+        }
+    }   
 }
