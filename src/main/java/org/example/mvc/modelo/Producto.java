9943a2cf6d3b4f607b172c3a7c1a4296aa3ac1b4
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-14 18:03:33 -0500

    commit-Producto .

--- src/main/java/org/example/mvc/modelo/Producto.java
+++ src/main/java/org/example/mvc/modelo/Producto.java
@@ -1,4 +1,69 @@
 package org.example.mvc.modelo;
 
 public class Producto {
-}
+
+    private int id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Producto() {
+
+    }
+
+    public Producto(int id, String nombre, double precio, int cantidad) {
+        this.id = id;
+        this.nombre =  nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public Producto(String nombre, double precio, int cantidad)
+    {
+        this.nombre =  nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}   
