be10ee9b6611b701a2c6dfd4bec4b116e9f21358
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-15 15:18:46 -0500

    ProductoDAO / .

--- src/main/java/org/example/mvc/config/ConexionBD.java
+++ src/main/java/org/example/mvc/config/ConexionBD.java
@@ -33,5 +33,5 @@ public class ConexionBD {
 
             return null;
         }
-    }   
+    }
 }
--- src/main/java/org/example/mvc/dao/ProductoDAO.java
+++ src/main/java/org/example/mvc/dao/ProductoDAO.java
@@ -1,4 +1,41 @@
 package org.example.mvc.dao;
 
-public class ProductoDAO {
+import org.example.mvc.config.ConexionBD;
+import org.example.mvc.modelo.Producto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoDAO  {
+
+    // INSERTAR
+    public boolean insertar(Producto producto) {
+        String sql = """
+                INSERT INTO productos(nombre, precio, cantidad)
+                VALUES (?,?,?)
+                """;
+
+        try (Connection conexion = ConexionBD.obtenerConexion();
+             PreparedStatement statement =
+                     conexion.prepareStatement(sql)) {
+
+            statement.setString(1, producto.getNombre());
+            statement.setDouble(2, producto.getPrecio());
+            statement.setInt(3, producto.getCantidad());
+
+            statement.executeUpdate();
+
+            return true;
+        } catch (SQLException e) {
+
+            System.out.println("Error al insertar producto:");
+            System.out.println(e.getMessage());
+
+            return false;
+        }
+
+        // LISTAR .
+        
+    }
 }
