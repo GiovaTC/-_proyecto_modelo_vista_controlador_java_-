ba8eac17bbb710dbe5e2becabb4abf44e167a5b2
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-15 15:26:04 -0500

    ProductoDAO .

--- src/main/java/org/example/mvc/dao/ProductoDAO.java
+++ src/main/java/org/example/mvc/dao/ProductoDAO.java
@@ -34,8 +34,38 @@ public class ProductoDAO  {
 
             return false;
         }
-
-        // LISTAR .
-        
     }
+        // LISTAR .
+        public List<Producto> listar() {
+            List<Producto> productos = new ArrayList<>();
+
+            String sql = """
+                    SELECT id, nombre, precio, cantidad
+                    FROM productos
+                    ORDER BY id
+                    """;
+
+            try (Connection conexion = ConexionBD.obtenerConexion();
+                 PreparedStatement statement =
+                         conexion.prepareStatement(sql);
+                 ResultSet resultado = statement.executeQuery()) {
+
+                while ( resultado.next()) {
+                    Producto producto = new Producto();
+
+                    producto.setId(resultado.getInt("id"));
+                    producto.setNombre(resultado.getString("nombre"));
+                    producto.setPrecio(resultado.getDouble("precio"));
+                    producto.setCantidad(resultado.getInt("cantidad"));
+
+                    productos.add(producto);
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al listar productos: ");
+                System.out.println(e.getMessage());
+            }
+
+            return productos;
+        }
+        
 }
